d31c0414fe01274be0adb709fea9a79584c2074b
yu miura <curelemonade15@gmail.com>
2021-07-22 23:59:39 +0900

    postgres対応

--- backend/tweetable/evolutions/src/main/resources/evolutions/default/1.sql
+++ backend/tweetable/evolutions/src/main/resources/evolutions/default/1.sql
@@ -2,15 +2,15 @@
 
 -- !Ups
 
-CREATE TABLE User (
-    id bigint(20) NOT NULL AUTO_INCREMENT,
-    email varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
-    fullname varchar(255) NOT NULL,
-    isAdmin boolean NOT NULL,
-    PRIMARY KEY (id)
+CREATE TABLE users (
+  id  SERIAL  NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  fullname VARCHAR(255) NOT NULL,
+  isAdmin BOOLEAN NOT NULL,
+  PRIMARY KEY (id)
 );
 
 -- !Downs
 
-DROP TABLE User;
+DROP TABLE users;
